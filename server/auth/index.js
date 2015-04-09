5f6bdcbd571fbf95d1b9f0d42f574b2eb6dd92bd
jKey Lu <jkeylu@gmail.com>
2015-04-09 21:24:03 +0800

    yo angular-fullstack

---
+++ server/auth/index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var express = require('express');
+var passport = require('passport');
+var config = require('../config/environment');
+var User = require('../api/user/user.model');
+
+// Passport Configuration
+require('./local/passport').setup(User, config);
+require('./google/passport').setup(User, config);
+
+var router = express.Router();
+
+router.use('/local', require('./local'));
+router.use('/google', require('./google'));
+
+module.exports = router;
