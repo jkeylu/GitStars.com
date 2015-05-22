e53b52fb569728abf9356a01f1759b24ac366dc8
jKey Lu <jkeylu@gmail.com>
2015-05-22 13:57:39 +0800

    update jsonwebtoken to v5.0.1

--- deploymodify.sh
+++ deploymodify.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-sed "s/\(GITHUB_ID\): \".*\"/\1: \"$1\"/; s/\(GITHUB_SECRET\): \".*\"/\1: \"$2\"/" ecosystem.json5
+sed -i "s/\(GITHUB_ID\): \".*\"/\1: \"$1\"/; s/\(GITHUB_SECRET\): \".*\"/\1: \"$2\"/" ecosystem.json5
